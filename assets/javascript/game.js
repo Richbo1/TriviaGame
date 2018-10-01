69274065832280db7783da60787064d7cf42c1cf
richard <richard@guest-10-168-154-114.wireless.umn.edu>
2018-10-01 17:04:23 -0500

    <finnished up late homework>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,110 @@
+var card = $("#quiz-area");
 
-window.onload = function() {
 
-    
-   
-    $("#start").click(stopwatch.start);
-  };
-  
-  //  Variable that will hold our setInterval that runs the stopwatch
-  var intervalId;
-  
-  // prevents the clock from being sped up unnecessarily
-  var clockRunning = false;
-  
-  //  Our stopwatch object.
-  var stopwatch = {
-  
-    time: 0,
-    lap: 1,
-  
-    reset: function() {
-  
-      stopwatch.time = 0;
-      
-  
-      
-      $('#display').text("00:00")
-    },
-  
-    start: function() {
-  
-      //  TODO: Use setInterval to start the count here and set the clock to running.
-      if (!clockRunning) {
-        intervalId = setInterval(stopwatch.count,1000);
-        clockRunning = true;
-  
-      }
-      
-    },
-    stop: function() {
-        if (seconds==30){
-            clearInterval(intervalId);
-            clockRunning = false;
-            
-          }
-    },
-  
-   
-    count: function() {
-      console.log("tick!")
-      //  TODO: increment time by 1, remember we cant use "this" here.
-      stopwatch.time++;
-      //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
-      //        and save the result in a variable.
-      const formattedTime = stopwatch.timeConverter(stopwatch.time)
-      //  TODO: Use the variable you just created to show the converted time in the "display" div.
-      $("#display").text(formattedTime);
-    },
-  
-   
-  
-    timeConverter: function(t) {
-  
-      //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-      var minutes = Math.floor(t / 60);
-      var seconds = t - (minutes * 60);
-  
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-  
-      if (minutes === 0) {
-        minutes = "00";
-      }
-  
-      else if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-  
-      return minutes + ":" + seconds;
+var questions = [{
+  question: "Who is the leader of the legion?",
+  answers: ["Caesar", "Mr. House", "Legate Linaus", "Yes Man"],
+  correctAnswer: "Caesar"
+}, {
+  question: "Who tries to kill the courier in the beginning of the game?",
+  answers: ["Ringo", "Benny", "Boone", "Ed-e"],
+  correctAnswer: "Benny"
+}, {
+  question: "Where can you find Boone initially?",
+  answers: ["Goodsprings", "Novac", "The Strip", "Hidden Valley"],
+  correctAnswer: "Novac"
+}
+];
+
+
+var timer;
+
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 30,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
     }
-  };
-  
-  
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      card.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        card.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    card.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    card.html("<h2>All Done!</h2>");
+    card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    card.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
